3d0a0cedb05bc39e8da8524a305256afd2c50207
Mandy Tsai <mandy3152618@gmail.com>
2023-03-21 15:30:43 +0800

    add: vue 與 環境安裝

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-essential', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  overrides: [],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
+  rules: {
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+    'vue/multi-word-component-names': 'off',
+    'prettier/prettier': ['error'],
+  },
+};
