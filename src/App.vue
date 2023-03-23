4ef4b78c1f5474bf91e8655400cc6d824bef7463
Mandy Tsai <mandy3152618@gmail.com>
2023-03-23 18:33:19 +0800

    add: 切換 colorMode

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
 import { useTodoStore } from '@/store';
 import { computed } from 'vue';
+import { useTheme } from 'vuetify';
+
 const $todoStore = useTodoStore();
+const theme = useTheme();
+const toggleTheme = (): void => {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark';
+};
 </script>
 
 <template>
-  <div>
-    <v-icon icon="home" />
-    <v-btn>button</v-btn>
-    <v-text-field label="Label" variant="underlined"></v-text-field>
-    <todoList />
-  </div>
+  <v-app>
+    <v-container>
+      <v-icon icon="home" />
+      <v-btn @click="toggleTheme">toggle theme</v-btn>
+      <v-text-field label="Label" variant="underlined"></v-text-field>
+      <todoList />
+    </v-container>
+  </v-app>
 </template>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,8 +1,23 @@
 // https://vuetifyjs.com/en/getting-started/installation/#manual-steps
 import 'vuetify/styles';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
-import { createVuetify } from 'vuetify';
+import { createVuetify, ThemeDefinition } from 'vuetify';
 import { aliases, md } from 'vuetify/iconsets/md';
+
+const customTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    'c-black-300': '#333333',
+    'c-black-200': '#5E5D5D',
+    'c-black-100': '#8B8B8B',
+    'c-gray-200': '#BBBBBB',
+    'c-gray-100': '#F4F4F4',
+    'c-brown-200': '#BC9C73',
+    'c-brown-100': '#F4EDE5',
+    'c-red-100': '#A1001A',
+    'c-pink-100': '#FFC0CA',
+  },
+};
 
 export default createVuetify({
   icons: {
@@ -10,6 +25,12 @@ export default createVuetify({
     aliases,
     sets: {
       md,
+    },
+  },
+  theme: {
+    defaultTheme: 'customTheme',
+    themes: {
+      customTheme,
     },
   },
 });
