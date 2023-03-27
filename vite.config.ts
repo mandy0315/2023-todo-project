be83509255590963686e4da763ffe8510f434906
Mandy Tsai <mandy3152618@gmail.com>
2023-03-27 17:45:22 +0800

    fix: path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vuetify from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/todo_ts-project' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/todo_ts-project/' : '/',
   plugins: [
     Components({
       /* options */
