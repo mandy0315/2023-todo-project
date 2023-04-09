47e5582c263cd60d84ade9b32eaa47787aa3699a
Mandy Tsai <mandy3152618@gmail.com>
2023-04-09 12:48:15 +0800

    fix: 部署網址名稱

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vuetify from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/todo_ts-project/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/2023-todo-project/' : '/',
   plugins: [
     Components({
       /* options */
